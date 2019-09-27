463e564c8a31e1d95825317f8c9f4ce4b48e9684
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-09-27 17:24:30 -0500

    Flujo de una aplicación en Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Master-Angular';
+  titulo = 'Hola mundo desde Angular';
 }
