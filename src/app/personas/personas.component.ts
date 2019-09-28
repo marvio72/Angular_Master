2973345f2571bed29a54bb55e175c6a323fb6bae
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-09-27 20:29:31 -0500

    Aplicando estilos CSS a Angular

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -2,7 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-personas',
-  templateUrl: './personas.component.html'
+  templateUrl: './personas.component.html',
+  // llamando a un archivo
+  // styleUrls: ['./personas.component.css']
+  // ejecutarlo en linea
+  styles: [`
+    h1{
+      color: #000099;
+    }
+  `]
 })
 export class PersonasComponent {
 
